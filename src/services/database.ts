7fa152ba8816c399cec06b6d57af6c536346fc8e
vgo <vincent.godin@veolia.com>
2021-04-05 14:08:34 +0200

    unit test temprature

--- src/services/database.ts
+++ src/services/database.ts
@@ -1,26 +1,36 @@
-import { from } from 'rxjs';
+import { EMPTY, from } from 'rxjs';
 import { map, mergeAll, reduce } from 'rxjs/operators';
 import * as sqlite from 'sqlite3';
 import { Node, Config, User } from '../models/models';
 
 let db: sqlite.Database;
 
-export function init() {
+export function init(): Promise<void> {
     if (db != null) {
         return;
     }
+    return new Promise<void>((resolve) => {
     db = new sqlite.Database('therm.sqlite');
     db.run('CREATE TABLE IF NOT EXISTS node (id INTEGER PRIMARY KEY, nom TEXT UNIQUE ON CONFLICT IGNORE, created_at TEXT DEFAULT CURRENT_TIMESTAMP, \
         min INTERGER, max INTERGER, redFrom INTERGER, redTo INTERGER, yellowFrom INTERGER, yellowTo INTERGER, minorTicks INTERGER )')
       .run('CREATE TABLE IF NOT EXISTS temperature (id INTEGER PRIMARY KEY, nodeId NUMBER, value REAL, date TEXT DEFAULT CURRENT_TIMESTAMP)')
-      .run('CREATE TABLE IF NOT EXISTS user (id INTERGER PRIMARY KEY, mail TEXT, detectLimit TEXT, nodes TEXT)');
-
-      console.log('database initialized');
+      .run('CREATE TABLE IF NOT EXISTS user (id INTERGER PRIMARY KEY, mail TEXT, detectLimit TEXT, nodes TEXT)', err => {
+        if (err) { throw err; }
+        console.log('database initialized');
+        resolve();
+      });
+    });
 }
 
-export function close() {
-    db.close();
-    this.db = null;
+export function close(): Promise<void> {
+    return new Promise(resolve => {
+        db.close(err => {
+            if (err) { throw err ; }
+            console.log('database closed');
+            db = null;
+            resolve();
+        });
+    });
 }
 
 export function addNode(node: string): Promise<number> {
@@ -33,6 +43,24 @@ export function addNode(node: string): Promise<number> {
     return id$;
 }
 
+export function removeNode(id: number): Promise<void> {
+    const promise$ = new Promise<void>((resolve) =>
+        db.run(`DELETE FROM node WHERE id = ?`, id, function(err) {
+        if (err) { throw err; }
+        resolve();
+    }));
+    return promise$;
+}
+
+export function removeAllNodes(): Promise<void> {
+    const promise$ = new Promise<void>((resolve) =>
+        db.run(`DELETE FROM node `, function(err) {
+        if (err) { throw err; }
+        resolve();
+    }));
+    return promise$;
+}
+
 export function addTemperature(nodeId: number, value: number, date: Date): Promise<number> {
     const id$ = new Promise<number>((resolve) =>
     db.run(`INSERT INTO temperature (nodeId, value, date) VALUES (?,?,?)`, [nodeId, value, date], function(err) {
@@ -40,6 +68,15 @@ export function addTemperature(nodeId: number, value: number, date: Date): Promi
         resolve(this.lastID);
     }));
     return id$;
+}
+
+export function removeAllTemperature(nodeId: number): Promise<void> {
+    const promise$ = new Promise<void>((resolve) =>
+        db.run(`DELETE FROM temperature WHERE nodeId = ? `, nodeId, function(err) {
+        if (err) { throw err; }
+        resolve();
+    }));
+    return promise$;
 }
 
 export function getNodeId(node: string): Promise<number> {
@@ -105,7 +142,7 @@ export function getNodes(): Promise<Node[]> {
 
 export function getTemperatures(node: Node, dateFrom: Date, dateTo: Date): Promise<Node> {
     return new Promise<Node>(resolve => {
-        db.all('SELECT round(date) as date, value FROM temperature WHERE nodeId = ? AND ? <= date AND date < ?', [node.id, dateFrom, dateTo, ], (err , rows) => {
+        db.all('SELECT date as date, value FROM temperature WHERE nodeId = ? AND ? <= date AND date < ?', [node.id, dateFrom, dateTo, ], (err , rows) => {
             if (err) { throw err; }
 
             resolve({
--- src/tests/services/database.test.ts
+++ src/tests/services/database.test.ts
@@ -2,36 +2,151 @@ import * as Database from '../../services/database';
 import * as sqlite3 from 'sqlite3';
 import { open } from 'sqlite';  // required for async operations
 import * as fs from 'fs';
+import { delay } from 'rxjs/operators';
 
 describe('database service', function() {
-    it('init', async function() {
+    beforeAll(async () => {
         try {
             const stats = await fs.promises.stat('therm.sqlite');
             await fs.promises.rename('therm.sqlite', 'therm.sqlite.ori');
         } catch {}
-
-        await Database.init();
-        Database.close();
-
+    });
+    
+    afterAll(async () => {
         try {
-            const db = await open({
-                filename: 'therm.sqlite',
-                driver: sqlite3.Database
-            });
+            const stats = await fs.promises.stat('therm.sqlite.ori');
+            // await fs.promises.rm('therm.sqlite');
+            // await fs.promises.rename('therm.sqlite.ori', 'therm.sqlite');
+        } catch {}
+    });
+
+    
+    it('init', async function() {
+        await Database.init();
+        await Database.close();
+
+        const db = await open({
+            filename: 'therm.sqlite',
+            driver: sqlite3.Database
+        });
+        try {
             let result = await db.get('SELECT sql FROM sqlite_master WHERE name like ?', 'node');
             expect(result?.sql).toEqual(expect.anything());
             result = await db.get('SELECT sql FROM sqlite_master WHERE name like ?', 'temperature');
             expect(result?.sql).toEqual(expect.anything());
             result = await db.get('SELECT sql FROM sqlite_master WHERE name like ?', 'user');
             expect(result?.sql).toEqual(expect.anything());
-            
-              
+        } catch (err) {
+            fail(err)
+        } finally {
+            db.close();
+        }
+    });
+
+
+    it('add/select/delete node', async function() {
+        await Database.init();
+        await Database.removeAllNodes();
+        let id = await Database.addNode('test');
+        await Database.close();
+
+        try {
+            const db = await open({ filename: 'therm.sqlite', driver: sqlite3.Database });
+            let rows = await db.all('SELECT * FROM node');
+
+            expect(rows.length).toBe(1);
+            let row = rows[0];
+            expect(id).toEqual(expect.any(Number));
+            expect(row).toMatchObject({
+                id: expect.any(Number),
+                created_at: expect.any(String),
+                nom: 'test',
+                min: 0,
+                max: 60,
+                redFrom: 35,
+                redTo: 60,
+                yellowFrom: 25,
+                yellowTo: 35,
+                minorTicks: 5,
+            });
+            await db.close();
         } catch (err) {
             fail(err)
         }
 
-            
-        
-    })
+        await Database.init();
+        let node = await Database.getNode(id);
+        expect({id, nom: 'test', temperature: undefined}).toEqual(expect.objectContaining({id, nom: 'test', temperature: undefined}));
 
+        await Database.removeNode(id)
+        await Database.close();
+        const db = await open({ filename: 'therm.sqlite', driver: sqlite3.Database });
+        let rows = await db.all('SELECT * FROM node');
+        expect(rows.length).toBe(0);
+        await db.close();
+    });
+
+
+    it('add/select/delete temperature', async function() {
+        await Database.init();
+        let nodes = await Database.getNodes();
+        nodes.forEach(async n => {
+            await Database.removeAllTemperature(n.id);
+            await Database.removeNode(n.id)
+        });
+        
+        let id1 = await Database.addNode('test1');
+        Database.addTemperature(id1, 12, new Date('2021-04-05 12:34:56'));
+        Database.addTemperature(id1, 12, new Date('2021-04-05 12:34:57'));
+
+        let id2 = await Database.addNode('test2');
+        Database.addTemperature(id2, 13, new Date('2021-04-05 12:34:56'));
+        Database.addTemperature(id2, 13, new Date('2021-04-05 12:34:57'));
+        Database.addTemperature(id2, 13, new Date('2021-04-10 12:34:57'));
+
+        nodes = await Database.getNodesWithTemperatures(new Date('2021-04-05 00:00:00'), new Date('2021-04-05 23:59:59'))
+
+        await Database.close();
+
+        try {
+            expect(nodes).toEqual(expect.arrayContaining([
+                expect.objectContaining({
+                    id: 1,
+                    nom: 'test1',
+                    temperatures:  expect.arrayContaining([
+                        expect.objectContaining({
+                            value: 12, date: new Date('2021-04-05 12:34:56')
+                        }),
+                        expect.objectContaining({
+                            value: 12, date: new Date('2021-04-05 12:34:57')
+                        })
+                    ]),
+                }),
+                expect.objectContaining({
+                    id: 2,
+                    nom: 'test2',
+                    temperatures:  expect.arrayContaining([
+                        expect.objectContaining({
+                            value: 13, date: new Date('2021-04-05 12:34:56')
+                        }),
+                        expect.objectContaining({
+                            value: 13, date: new Date('2021-04-05 12:34:57')
+                        })
+                    ]),
+                })
+            ]));            
+
+            const db = await open({ filename: 'therm.sqlite', driver: sqlite3.Database });
+            let rows = await db.all('SELECT * FROM node');
+            expect(rows.length).toBe(2);
+            expect(rows[0]).toMatchObject({nom: 'test1'});
+            rows = await db.all('SELECT * FROM temperature');
+            expect(rows.length).toBe(5);
+            expect(rows[0]).toMatchObject({value: 12});
+            
+            db.close();
+        } catch(err) {
+            fail(err);
+        }
+    });
 });
